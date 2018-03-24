b3f03e4e0ea7737f1f8be6aacbf9b023f39b1fbc
Martijn Pieters <mail@martijnpieters.nl>
2018-03-24 14:48:45 +0100

    Updated dependencies

--- README.md
+++ README.md
@@ -5,13 +5,22 @@
 ## Prerequisites
 
 - This project relies on a docker installation of WPScan, see their [documentation](https://github.com/wpscanteam/wpscan#docker) for installation instructions.
-- `php >= 5.5.9`
+- `php >= 7.0`
 
 ## Installation
 
 Install this repository through [Composer](http://getcomposer.org/):
 ```
-composer install
+$ composer install
 ```
 
 After retrieving the required packages, point your favourite webserver to the `app/` directory and start using the hosted WPScan.
+
+## Run development server
+
+Run PHP's built-in development server with:
+```bash
+$ php -S 127.0.0.1:5000 app/index.php
+```
+
+After that, point your web browser to [http://127.0.0.1:5000](http://127.0.0.1:5000) and play with it. 😄
--- app/index.php
+++ app/index.php
@@ -1,87 +1,97 @@
 <?php
+
+use Silex\Application;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Form;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Process\Process;
-use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Validator\Constraints as Assert;
 
-require_once(__DIR__.'/../vendor/autoload.php');
+require_once(__DIR__ . '/../vendor/autoload.php');
 
-$app = new Silex\Application();
+const PROCESS_COMMAND_DOCKER_WPSCAN = 'docker run --rm wpscanteam/wpscan -u %s --update';
+const PROCESS_OUTPUT_COLOR_REPLACEMENTS = [
+    '[31m' => '<span class="text-danger">',
+    '[32m' => '<span class="text-success">',
+    '[33m' => '<span class="text-warning">',
+    '[34m' => '<span class="text-info">',
+    '[0m' => '</span>',
+];
+
+$app = new Application();
 $app['debug'] = true;
 
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
+$app->register(new Silex\Provider\TwigServiceProvider(), [
+    'twig.path' => __DIR__ . '/views',
+]);
 $app->register(new Silex\Provider\FormServiceProvider());
 $app->register(new Silex\Provider\LocaleServiceProvider());
 $app->register(new Silex\Provider\ValidatorServiceProvider());
-$app->register(new Silex\Provider\TranslationServiceProvider(), array(
-    'translator.domains' => array(),
-    'locale_fallbacks' => array('en'),
-));
+$app->register(new Silex\Provider\TranslationServiceProvider(), [
+    'translator.domains' => [],
+    'locale_fallbacks' => ['en'],
+]);
 
-function getUrlForm() {
-    global $app;
+function getUrlForm(Application $app): Form
+{
     return $app['form.factory']->createNamedBuilder(null, FormType::class)
-        ->add('url', UrlType::class, array(
-            'attr' => array('placeholder' => 'URL starting with http:// or https://'),
-            'constraints' => array(new Assert\Url()),
+        ->add('url', UrlType::class, [
+            'attr' => ['placeholder' => 'URL starting with http:// or https://'],
+            'constraints' => [new Assert\Url()],
             'required' => true,
-        ))
-        ->add('submit', SubmitType::class, array(
+        ])
+        ->add('submit', SubmitType::class, [
             'label' => 'Scan',
-            'attr' => array('class' => 'btn-primary'),
-        ))
+            'attr' => ['class' => 'btn-primary'],
+        ])
         ->setMethod('GET')
         ->getForm();
 }
 
 $app->get('/', function (Request $request) use ($app) {
-    $form = getUrlForm();
+    $form = getUrlForm($app);
     $form->handleRequest($request);
+
     if ($form->isValid()) {
         $data = $form->getData();
     }
-    
-    return $app['twig']->render('index.twig', array(
+
+    return $app['twig']->render('index.twig', [
         'form' => $form->createView(),
         'url' => $data['url'] ?? null,
-    ));
+    ]);
 });
 
 $app->get('/scan', function (Request $request) use ($app) {
-    $form = getUrlForm();
+    $form = getUrlForm($app);
     $form->handleRequest($request);
+
     if ($form->isValid()) {
         $data = $form->getData();
         $url = $data['url'];
 
-        $process = new Process('docker run --rm wpscanteam/wpscan -u ' . $url . ' --update');
+        $process = new Process(vsprintf(PROCESS_COMMAND_DOCKER_WPSCAN, [$url]));
         $process->run();
-        
-        $dictionary = array(
-            '[31m' => '<span class="text-danger">',
-            '[32m' => '<span class="text-success">',
-            '[33m' => '<span class="text-warning">',
-            '[34m' => '<span class="text-info">',
-            '[0m'   => '</span>' ,
+
+        $wpscanOutput = $process->getOutput();
+        $wpscanOutputHtmlColors = str_replace(
+            array_keys(PROCESS_OUTPUT_COLOR_REPLACEMENTS),
+            PROCESS_OUTPUT_COLOR_REPLACEMENTS,
+            $wpscanOutput
         );
 
-        return new JsonResponse(array(
+        return new JsonResponse([
             'url' => $url,
-            'errors' => substr_count($wpscan, '[31m'),
-            'warnings' => substr_count($wpscan, '[33m'),
-            'output' => str_replace(array_keys($dictionary), $dictionary, $process->getOutput()),
-        ));
+            'errors' => substr_count($wpscanOutput, '[31m'),
+            'warnings' => substr_count($wpscanOutput, '[33m'),
+            'output' => $wpscanOutputHtmlColors,
+        ]);
     } else {
         $app->abort(500, 'Given URL is not valid');
     }
-})
-->bind('scan');
+})->bind('scan');
 
 $app->run();
